350af71b53ec31a1a83242291a40a9e5e0b75a60
JohnMukendi <101872863+JohnMukendi@users.noreply.github.com>
2022-11-09 12:45:05 +0000

    added images

---
+++ pages/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Box from '@mui/material/Box';
+
+function About() {
+  return (
+    <Box  className='aboutus'>
+        <Box style={{backgroundImage:`url('./City on hill About us 2 1.png.png')`,height:'100vh',}} className='displayText'>About Us</Box>
+    </Box>
+  )
+}
+
+export default About
