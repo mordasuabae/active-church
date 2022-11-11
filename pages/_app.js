d46ba261a2c99bac2dba8f64daca5df96cc7509b
Grasykisibu <kisibugrady3980@gmail.com>
2022-11-11 09:14:55 +0200

    pushing banner section

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
+import Layout from '../components/Layout'
+import Navbar from '../components/Navbar'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+            
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Banner from '../components/Banner'
 import styles from '../styles/Home.module.css'
+import { Box } from '@mui/material'
 
 export default function Home() {
   return (
-   <div>
-    
-   </div>
+   <Box>
+    <Banner/>
+
+   </Box>
   )
 }
