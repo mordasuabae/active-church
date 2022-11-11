190bd33376989f71261add78731b00ffb88e368a
JohnMukendi <mukendijohn937@gmail.com>
2022-11-11 09:10:50 +0200

    pushing for presentation

---
+++ components/homepage/data.js
@@ -0,0 +1,34 @@
+export const data = [
+    {
+        Title : 'Art of War',
+        type : 'Sunday Service',
+        time : '10:00 am - 11:00am',
+        thumbnail : 'https://i.picsum.photos/id/211/200/300.jpg?hmac=wrwgBoS1KPKiLCrxowMtMQ7NpVlzI1NvocRSpH6HSm0'
+    },
+    {
+        Title : 'Major Event',
+        type : "Active Youth",
+        time : '18:00pm - 19:00pm',
+        thumbnail : 'https://i.picsum.photos/id/793/200/300.jpg?hmac=MOZTy7CEiCptTmTIOiss-6dGsFhhfJPMTKyTgyR12hw'
+    },
+    {
+        Title : 'Are you up babes',
+        type : 'Bible Studies',
+        time : '10:00 am - 11:00am',
+    },
+    {
+        Title : 'Trust Again',
+        type : 'Active Worship',
+        time : '04 : 20',
+    },
+    {
+        Title : 'Adam and Eve',
+        type : "Children's Church",
+        time : '10:00 am - 11:00am',
+    },
+    {
+        Title : 'Debate Night',
+        type : 'Active Youth',
+        time : '18:00pm - 19:00pm'
+    }    
+]
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Events from '../components/homepage/events'
 
 export default function Home() {
   return (
    <div>
-    
+    <Events />
+      
    </div>
   )
 }
